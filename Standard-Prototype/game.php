4137c97d98c0f3646fad0f95838df2a80377a5cd
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-10-07 09:28:34 +0200

    fix diff choices

--- Standard-Prototype/game.php
+++ Standard-Prototype/game.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/ab021e0629.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/955adab269.js" crossorigin="anonymous"></script>
     <script>
         let difficulty = "<?php
                             if (isset($_POST["hard"])) {
